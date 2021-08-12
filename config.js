aa77c0935bd231a9b39928221ffd4c9fb5d274a0
wanmd <1690014812@qq.com>
2021-08-12 19:55:57 +0800

    提交

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 let config = {
     baseUrls: 'https://r.kingwish.com.cn/api/',
-    baseUrls: 'https://dev.kingwish.com.cn/api/',
+    // baseUrls: 'https://dev.kingwish.com.cn/api/',
     userinfo_key: 'userinfo',
     appId: 'wx3020976f686fe9f8',
     appData: {
-        appName: 'iME爱迷',
+        appName: 'iYOU',
         wxinfo_key: 'wxInfo',
         defaultImglogo: 'https://x.wxzs.co/x/4/img-m.png'
     }
